e4a478a033b0cb55d074bf4d23d9215dabe4b79f
Francisco Payes <payesfrancisco@gmail.com>
2017-01-13 00:21:09 -0400

    SKP: Add score funcionality

--- ScoreKeeper/app/src/main/java/com/example/intellisys/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/intellisys/scorekeeper/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.intellisys.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Integer teamAPunctuation = 0;
+    Integer teamBPunctuation = 0;
+    TextView scoreA;
+    TextView scoreB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreA = (TextView) findViewById(R.id.scoreA);
+        scoreB = (TextView) findViewById(R.id.scoreB);
+        scoreA.setText(teamAPunctuation.toString());
+        scoreB.setText(teamBPunctuation.toString());
+    }
+
+    public void addPointToScore(View view) {
+        String tag = view.getTag().toString();
+        String team = tag.split(",")[0];
+        int points = Integer.parseInt(tag.split(",")[1]);
+
+        if (team.equals("a")) {
+            teamAPunctuation += points;
+            scoreA.setText(teamAPunctuation.toString());
+        } else {
+            teamBPunctuation += points;
+            scoreB.setText(teamBPunctuation.toString());
+        }
+    }
+
+    public void resetPunctuation(View view) {
+        teamAPunctuation = 0;
+        teamBPunctuation = 0;
+        scoreA.setText(teamAPunctuation.toString());
+        scoreB.setText(teamBPunctuation.toString());
     }
 }
